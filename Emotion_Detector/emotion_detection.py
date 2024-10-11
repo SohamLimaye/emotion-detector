32472b850c778b176fd70c375cc41ec6c4f4522e
SohamLimaye <soham.22111058@viit.ac.in>
2024-10-11 14:50:53 +0530

    completed required tasks

---
+++ Emotion_Detector/emotion_detection.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, headers=headers, json=input_json)
+    
+    formatted_response = json.loads(response.text)
+    
+    if response.status_code == 200:
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']    
+        # Determine the dominant emotion
+        dominant_emotion = max(emotions, key=emotions.get)
+    
+    elif response.status_code == 400:
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+        dominant_emotion = None
+    
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
